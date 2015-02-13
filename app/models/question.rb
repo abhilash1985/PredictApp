00665e9f7fbc113420e87d42d90465a495fd919b
Abhilash V <abhivannarath006@gmail.com>
2015-02-13 18:31:27 +0530

    Added pg Gem

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,7 +10,7 @@ class Question < ActiveRecord::Base
     when 'Who will win the toss', 'Who will be batting first'
       { v: %W(#{match.team1_short_name} #{match.team2_short_name}) }
     when 'Runs will be scored by the team batting first', 'Runs will be scored by the team batting second'
-      { v: %w(<100 100-200 200-300 300-400 400<) }
+      { v: %w(<100 100-175 176-225 226-280 281-350 350<) }
     else
       { v: %w() }
     end
