ea07dedaf30699c4a562c977d41e7c8d47d7cdee
Abhilash V <abhivannarath006@gmail.com>
2015-02-20 08:56:56 +0530

    changes in master data

--- app/models/match_question.rb
+++ app/models/match_question.rb
@@ -11,9 +11,9 @@ class MatchQuestion < ActiveRecord::Base
     def add_match_questions
       Match.order(:id).each do |match|
         all_questions = [Question.defaults,
-                         Question.others.order('rand()').limit(3),
-                         Question.outs.order('rand()').limit(1),
-                         Question.power_plays.order('rand()').limit(1),
+                         Question.others.offset(rand(7)).limit(3),
+                         Question.outs.offset(rand(3)).limit(1),
+                         Question.power_plays.offset(rand(6)).limit(1),
                          Question.team_score(match.team1_short_name.upcase),
                          Question.team_score(match.team2_short_name.upcase)]
         all_questions.each do |questions|
