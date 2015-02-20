142b3e6f4324f9443676a41d874a07b13c1a2f32
Abhilash V <abhivannarath006@gmail.com>
2015-02-20 14:06:17 +0530

    changes in prediction

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,14 +7,17 @@ class TournamentsController < ApplicationController
 		@prediction = Prediction.new
 	end
 
-	def predict_match
-		@challenge_id, @match_id = params[:challenge_id], params[:match_id]
-		user_challenge = current_user.user_challenges.by_challenge(params[:challenge_id]).first_or_initialize
-		user_challenge.save
-		params[:match_question].each do |key, value|
-			prediction = user_challenge.predictions.by_match_question(key).first_or_initialize
-      prediction.user_answer = value
-      prediction.save
+  def predict_match
+    @challenge_id, @match_id = params[:challenge_id], params[:match_id]
+    match = Match.find_by_id(@match_id)
+    if match && !match.started?
+		  user_challenge = current_user.user_challenges.by_challenge(params[:challenge_id]).first_or_initialize
+		  user_challenge.save
+		  params[:match_question].each do |key, value|
+			  prediction = user_challenge.predictions.by_match_question(key).first_or_initialize
+        prediction.user_answer = value
+        prediction.save
+		  end
 		end
 		respond_to do |format|
 			format.js
