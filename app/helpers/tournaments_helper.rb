5b6ba07c609bc6403e6dc198a2bd1b4f8314bf5a
Abhilash V <abhivannarath006@gmail.com>
2015-03-17 20:22:53 +0530

    Renamed leaderboard title

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,14 +1,13 @@
 module TournamentsHelper
+  def load_icon
+    ['icon1.jpg', 'icon2.jpg', 'icon3.jpg', 'icon4.jpg', 'icon5.jpg', 'icon6.jpg'].sample
+  end
 
-	def load_icon
-		['icon1.jpg', 'icon2.jpg', 'icon3.jpg', 'icon4.jpg', 'icon5.jpg', 'icon6.jpg'].sample
-	end
-
-	def option_collection(match)
-  	team1_players = match.team_players('team1')
+  def option_collection(match)
+    team1_players = match.team_players('team1')
     team2_players = match.team_players('team2')
-	  [[match.team1_name, team1_players]] + [[match.team2_name, team2_players]]
-	end
+    [[match.team1_name, team1_players]] + [[match.team2_name, team2_players]]
+  end
 
   def options_hash(page_from, match_question, prediction)
     all_options = {}
@@ -16,5 +15,9 @@ module TournamentsHelper
     all_options[:match_question] = match_question
     all_options[:prediction] = prediction
     all_options
+  end
+
+  def leaderboard(from = nil)
+    from.blank? ? 'Leaderboard' : 'Knockout Board'
   end
 end
