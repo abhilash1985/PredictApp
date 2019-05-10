617290d4f8a3d89e37cce6b37d2a158460c669ef
Abhilash V <abhilash.vannarath@navigantbpm.com>
2019-05-11 01:25:59 +0530

    Added optional: true for models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,4 @@
 # Payment
 class Payment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 end
--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,4 +1,4 @@
 # Prize
 class Prize < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 end
