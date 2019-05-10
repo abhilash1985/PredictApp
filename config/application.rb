cd4161402967c42feff9d93f0607cdb784a2dcbb
Abhilash V <abhilash.vannarath@navigantbpm.com>
2019-05-11 01:42:36 +0530

    Updated default layout & config.ru

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module PredictApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Chennai'
+    # config.time_zone = 'Chennai'
   end
 end
--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -23,3 +23,6 @@ Rails.application.config.active_record.belongs_to_required_by_default = false
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 ActiveSupport.halt_callback_chains_on_return_false = true
+
+# Default Timezone
+Rails.application.config.time_zone = 'Chennai'
