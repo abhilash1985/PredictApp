4e9f9a002ced0e4889a93de8740dce4803efeb0f
Abhilash V <abhilash.vannarath@navigantbpm.com>
2019-05-11 01:51:58 +0530

    Removed unwanted file

--- config/initializers/setup_email.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  authentication: :plain,
-  user_name: 'cricketfever2015@gmail.com',
-  password: '123qweAadmin',
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'gmail.com',
-  enable_starttls_auto: true
-}
