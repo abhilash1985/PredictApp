4eb50a1f00db19754694d6cc776414253a07924a
Abhilash V <abhilash.vannarath@navigantbpm.com>
2019-05-11 01:07:18 +0530

    Fixed Devise login issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_filter :authenticate_user!
   before_action :current_tournament
 
@@ -21,6 +21,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :middle_name, :last_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :middle_name, :last_name])
   end
 end
