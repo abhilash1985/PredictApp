83a99298a768763ff7989ded53b1e7ffd3a965fe
Abhilash V <abhilash.vannarath@navigantbpm.com>
2019-05-12 14:21:29 +0530

    Upgraded to Rails 5.1

--- config/initializers/new_framework_defaults_5_1.rb
+++ config/initializers/new_framework_defaults_5_1.rb
@@ -12,3 +12,6 @@ Rails.application.config.action_view.form_with_generates_remote_forms = false
 # Unknown asset fallback will return the path passed in when the given
 # asset is not present in the asset pipeline.
 # Rails.application.config.assets.unknown_asset_fallback = false
+
+# Default Timezone
+Rails.application.config.time_zone = 'Chennai'
