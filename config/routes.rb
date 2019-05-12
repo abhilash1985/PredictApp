00c33c83320316c09382f2da2af39796c808dd01
Abhilash V <abhilash.vannarath@navigantbpm.com>
2019-05-12 14:40:41 +0530

    Updated Rails App - 5.1 to 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-#  Routes.rb
+# Rails routes
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/sign_in', to: 'dashboard#welcome'
