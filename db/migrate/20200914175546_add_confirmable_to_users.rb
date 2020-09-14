68890716f4e1fc941cd1d5a5b522bde68c8fdf5e
Abhilash V <abhilash.vannarath@navigantbpm.com>
2020-09-14 23:48:33 +0530

    Add confirmable to users

---
+++ db/migrate/20200914175546_add_confirmable_to_users.rb
@@ -0,0 +1,15 @@
+# Add Confirmable Module To Users
+class AddConfirmableToUsers < ActiveRecord::Migration[6.0]
+  def change
+    change_table(:users) do |t|
+      # Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+    end
+
+    add_index :users, :confirmation_token, unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_152614) do
+ActiveRecord::Schema.define(version: 2020_09_14_175546) do
 
   create_table "challenge_payments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -213,6 +213,11 @@ ActiveRecord::Schema.define(version: 2020_02_22_152614) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
