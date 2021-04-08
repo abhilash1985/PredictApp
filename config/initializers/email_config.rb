b3a48615f2ce9164031bdad98fbf3ca6fcc9fcf8
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-04-09 00:18:36 +0530

    Mail Configuration

--- config/initializers/email_config.rb
+++ config/initializers/email_config.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.delivery_method = :letter_opener # :smtp
+ActionMailer::Base.delivery_method = :smtp # :letter_opener # :smtp
 
-# ActionMailer::Base.smtp_settings = {
-#   authentication: :plain,
-#   user_name: Rails.application.credentials.mail_config[:user_name],
-#   password: Rails.application.credentials.mail_config[:password],
-#   address: Rails.application.credentials.mail_config[:address],
-#   port: Rails.application.credentials.mail_config[:port],
-#   domain: Rails.application.credentials.mail_config[:domain],
-#   enable_starttls_auto: true
-# }
+ActionMailer::Base.smtp_settings = {
+  authentication: :plain,
+  user_name: Rails.application.credentials.mail_config[:user_name],
+  password: Rails.application.credentials.mail_config[:password],
+  address: Rails.application.credentials.mail_config[:address],
+  port: Rails.application.credentials.mail_config[:port],
+  domain: Rails.application.credentials.mail_config[:domain],
+  enable_starttls_auto: true
+}
 
 LetterOpener.configure do |config|
   # To overrider the location for message storage.
