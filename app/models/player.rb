bc8ff3ab6917c8d9276a39b11d84556e36bef9fa
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-09-19 01:34:11 +0530

    Sort players

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,8 +7,8 @@ class Player < ApplicationRecord
   # Scopes
   scope :by_first_name, ->(first_name) { where(first_name: first_name) }
   scope :by_last_name, ->(last_name) { where(last_name: last_name) }
-  scope :order_by_captain, -> { order('last_name desc, first_name') }
-  scope :not_withdrawn, -> { where("player_style not like '%withdrawn%'") }
+  scope :order_by_captain, -> { order('last_name asc, first_name') }
+  scope :not_withdrawn, -> { where.not('player_style like ?', '%Withdrawn%') }
 
   def full_name
     "#{first_name} (#{player_style.upcase})".strip
