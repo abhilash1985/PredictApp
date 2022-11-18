0c53b1062923856adb31674104c58f401825f66e
abhilash_vannarath <abhilash.vannarath@iqvia.com>
2022-11-18 16:37:38 +0530

    Added Change Password

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@
 
 # DashboardController
 class DashboardController < ApplicationController
-  before_action :authenticate_user!, except: [:welcome]
+  before_action :authenticate_user!, except: [:welcome, :change_my_password]
   skip_before_action :current_tournament
   layout 'login'
 
@@ -14,5 +14,27 @@ class DashboardController < ApplicationController
 
   def index
     @tournaments = Tournament.all
+  end
+
+  def change_my_password
+    @params = params.require(:user).permit(:email, :password, :password_confirmation)
+    user = User.find_by(email: @params[:email])
+    if user.nil?
+      redirect_to new_user_password_path, notice: I18n.t('devise.failure.email_not_found')
+    elsif @params[:password] == @params[:password_confirmation]
+      update_password(user, @params)
+    else
+      redirect_to new_user_password_path, notice: I18n.t('devise.failure.password_not_match')
+    end
+  end
+
+  private
+
+  def update_password(user, params)
+    if user.update(password: params[:password])
+      redirect_to root_path, notice: I18n.t('devise.passwords.updated_not_active')
+    else
+      redirect_to new_user_password_path, notice: user.errors.full_messages.join(', ')
+    end
   end
 end
